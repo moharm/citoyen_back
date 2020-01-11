be1d49ae6f23a12c5e9a67089e40706a25c4a9d9
unknown <moharm123@gmail.com>
2020-01-11 13:41:18 +0100

    start readme file to explain APIs

--- src/main/java/com/back/back_citoyen/DAO/association/ActiviteRepo.java
+++ src/main/java/com/back/back_citoyen/DAO/association/ActiviteRepo.java
@@ -5,8 +5,9 @@ import java.util.List;
 import com.back.back_citoyen.Entity.Assosiation.Activite;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 /**
  * ActiviteRepo
@@ -16,5 +17,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 public interface ActiviteRepo extends JpaRepository<Activite, Long> {
 
     public List<Activite> findByAssosiation_Id(Long id);
+
+    @RestResource(path = "/ByStatus")
+    public List<Activite> findByStatut(@Param("status") String statut);
 
 }
