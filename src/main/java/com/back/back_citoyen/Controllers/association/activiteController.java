5ccdc28de7b70a8045919e6ba8d6bda53f9bee04
younes-139 <younes.moumad@edu.uca.ma>
2020-01-14 15:16:48 +0100

    after pull

--- src/main/java/com/back/back_citoyen/Controllers/association/activiteController.java
+++ src/main/java/com/back/back_citoyen/Controllers/association/activiteController.java
@@ -8,6 +8,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -45,9 +46,10 @@ public class activiteController {
 
     }
 
-    @GetMapping(value = "/activities")
-    public List<Activite> Liste() {
-        List<Activite> liste = activiteRepo.findAll();
+    @CrossOrigin("*")
+    @GetMapping(value = "/activites/inProgress")
+    public List<Activite> ListeInProgress() {
+        List<Activite> liste = activiteRepo.findByStatut("in progress");
 
         return liste;
     }
