5e6f4f6aec7c92b3bad4f10cc088483b9d03c793
unknown <moharm123@gmail.com>
2020-01-14 20:13:46 +0100

    upload images from android

---
+++ src/main/java/com/back/back_citoyen/Controllers/citoyen/CitoyenSetting.java
@@ -0,0 +1,38 @@
+package com.back.back_citoyen.Controllers.citoyen;
+
+import java.io.IOException;
+
+import com.back.back_citoyen.DAO.association.ActiviteRepo;
+import com.back.back_citoyen.DAO.association.AssociationRepo;
+import com.back.back_citoyen.DAO.citoyen.CitoyenRepo;
+import com.back.back_citoyen.Entity.Assosiation.assosiation;
+import com.google.gson.JsonObject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.http.MediaType;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/**
+ * CitoyenSetting
+ */
+@RestController
+public class CitoyenSetting {
+
+    CitoyenRepo citoyenRepo;
+
+    @GetMapping(path = "/photoCitoyen/{id}" , produces = MediaType.IMAGE_PNG_VALUE)
+    public byte[] getPhoto(@PathVariable("id")  Long id) throws IOException {
+        String image = citoyenRepo.findById(id).get().getImage();
+        return Files.readAllBytes(Paths.get("../Store/Citoyen/"+image));
+    }
+    
+}
