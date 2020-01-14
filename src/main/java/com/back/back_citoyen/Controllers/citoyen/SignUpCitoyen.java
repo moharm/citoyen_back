86c8de7e0a51577889860d3770f121b49aa04a05
unknown <moharm123@gmail.com>
2020-01-14 21:11:40 +0100

    citoyen signup

--- src/main/java/com/back/back_citoyen/Controllers/citoyen/SignUpCitoyen.java
+++ src/main/java/com/back/back_citoyen/Controllers/citoyen/SignUpCitoyen.java
@@ -4,7 +4,9 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import com.back.back_citoyen.DAO.association.AssociationRepo;
+import com.back.back_citoyen.DAO.citoyen.CitoyenRepo;
 import com.back.back_citoyen.Entity.Assosiation.assosiation;
+import com.back.back_citoyen.Entity.Citoyen.Citoyen;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -17,28 +19,38 @@ import org.springframework.web.multipart.MultipartFile;
 /**
  * SignUpCitoye
  */
+@CrossOrigin("*")
 @RestController
 public class SignUpCitoyen {
 
-    // @CrossOrigin("*")
-    // @PostMapping(value = "/citoyen/signup")
-    // public String Signup(@RequestParam(value = "image", required = false) MultipartFile image,
-    //                         @RequestParam("prenom") String prenom,
-    //                         @RequestParam("nom") String nom,
-    //                         @RequestParam("mdp") String mdp,
-    //                         @RequestParam("ville") String ville,
-    //                         @RequestParam("Adresse1") String Adresse1,
-    //                         @RequestParam("Adresse2") String Adresse2,
-    //                         @RequestParam("Tel") String Tel,
-    //                         @RequestParam("email") String email,
-    //                         @RequestParam("email") String image,
-    //                         @RequestParam("email") Long age) throws IOException {
-                                
+    @Autowired
+    CitoyenRepo citoyenRepo;
 
-    //     System.out.println(image.getContentType().split("/")[1]);
-     
-    //     Files.write(Paths.get("../Store/Citoyen/" + image.getOriginalFilename()), image.getBytes());
-    //     return "prenom";
-    // }
+    @PostMapping(value = "/citoyen/signup")
+    public String Signup(@RequestParam(value = "image", required = false) MultipartFile image,
+                            @RequestParam("prenom") String prenom,
+                            @RequestParam("nom") String nom,
+                            @RequestParam("mdp") String mdp,
+                            @RequestParam("ville") String ville,
+                            @RequestParam("adresse1") String adresse1,
+                            @RequestParam("adresse2") String adresse2,
+                            @RequestParam("tel") String tel,
+                            @RequestParam("email") String email,
+                            @RequestParam("age") Long age) throws IOException {
+                                
+            System.out.println(image.getContentType().split("/")[1]);
+            // Long id =associationRepo.count()+1;
+            Citoyen citoyen = new Citoyen(nom, prenom, mdp, ville, adresse1, adresse2, tel, email, age);
+            Long id = citoyenRepo.save(citoyen).getId();
+            citoyen.setId(id);
+            citoyen.setImage(id + "." + image.getContentType().split("/")[1]);
+            citoyenRepo.save(citoyen);
+            Files.write(Paths.get("../Store/Citoyen/" + citoyen.getImage()), image.getBytes());
+            if (citoyenRepo.save(citoyen) != null) {
+                return "seccess";
+            }
+            return "failed";
+       
+    }
 
 }
